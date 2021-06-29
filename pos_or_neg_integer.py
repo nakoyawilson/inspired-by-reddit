fece79eb244075b5b35883d614c5b0cab8fe695a
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-29 18:23:08 -0400

    Fix code to count integers and stop infinite loop

--- pos_or_neg_integer.py
+++ pos_or_neg_integer.py
@@ -6,13 +6,13 @@ def is_negative(number):
     if number < 0:
         return True
 
+pos_count = 0
+neg_count = 0
 continue_program = True
 while continue_program:
     number = int(input("Please enter a positive or negative integer, or 0 to quit: "))
-    pos_count = 0
-    neg_count = 0
     if number == 0:
-        conitnue_program = False
+        continue_program = False
         print(f"You entered {pos_count} positive integer(s) and {neg_count} negative integer(s).")
     elif number > 100 or number < -100:
         print("Invalid input. Please make sure your integer is between -100 and 100.")
