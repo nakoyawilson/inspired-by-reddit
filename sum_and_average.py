4cc8766b0c80035270494296125c73ca3db0aae2
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-03 21:09:49 -0400

    Fix type error when function called multiple times

--- sum_and_average.py
+++ sum_and_average.py
@@ -1,7 +1,7 @@
 def sum_and_average(list_of_numbers):
     """Takes a list of numbers and returns the sum and average"""
     num_list = []
-    for index, num in enumerate(str_num_list):
+    for num in str_num_list:
         num = float(num)
         num_list.append(num)
     len_of_list = len(num_list)
@@ -15,8 +15,8 @@ while continue_program == "y":
     continue_program = input("Do you want to submit a list of numbers? Type 'y' or 'n': ")
     if continue_program == "y":
         str_num_list = input("Enter a list of numbers separated by spaces. Press enter when you are finished.\n").split()
-        sum, average = sum_and_average(str_num_list)
-        print(f"The sum of the numbers you entered is: {sum}")
-        print(f"The average of the numbers you entered is: {average}")
+        sum_nums, average_nums = sum_and_average(str_num_list)
+        print(f"The sum of the numbers you entered is: {sum_nums}")
+        print(f"The average of the numbers you entered is: {average_nums}")
     else:
         print("Goodbye")
