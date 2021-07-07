6f928fd704814bb98cb0d013db9b028b21bcb1e9
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-07 14:57:19 -0400

    Create in_order() function and fix typos in starter code

--- in_order.py
+++ in_order.py
@@ -1,8 +1,16 @@
 def in_order(nums):
     # Type Your code here.
+    new_list =[]
+    for num in nums:
+        new_list.append(num)
+    new_list.sort()
+    if nums == new_list:
+        return True
+    else:
+        return False
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     # Test out-of-order example
     nums1 = [5, 6, 7, 8, 3]
     if in_order(nums1):
@@ -12,7 +20,7 @@ if __name__ = '__main__':
 
     # Test in-order example
     nums2 = [5, 6, 7, 8, 10]
-    if in_order(nums1):
+    if in_order(nums2):
         print('In order')
     else:
         print('Not in order')
