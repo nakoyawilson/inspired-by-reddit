f37afb49c8c19c6af3daf331bb5edff7f339ba15
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-07 14:39:04 -0400

    Add starter code

---
+++ in_order.py
@@ -0,0 +1,11 @@
+def in_order(nums):
+    # Type Your code here.
+
+
+if __name__ = '__main__':
+    # Test out-of-order example
+    nums1 = [5, 6, 7, 8, 3]
+    if in_order(nums1):
+        print('In order')
+    else:
+        print('Not in order')
