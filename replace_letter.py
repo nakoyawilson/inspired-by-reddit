f8b9deb9bbd7e4409484631ea533de6b4fdcbfe6
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-07 22:34:46 -0400

    Create replace function and add test

--- replace_letter.py
+++ replace_letter.py
@@ -1,2 +1,13 @@
+def replace(word, letter_to_replace, replacement):
+    new_word = ""
+    for letter in word:
+        if letter == letter_to_replace:
+            new_word += replacement
+        else:
+            new_word += letter
+    return new_word
 
-            
+
+# Test            
+new_word = replace("hollar", "h", "d")
+print(new_word)
