3328e4e2baa4a0f225394c6de0b347510e2bae74
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-09 19:11:13 -0400

    Add test

--- create_phone_number.py
+++ create_phone_number.py
@@ -4,3 +4,9 @@ def create_phone_number(list_of_numbers):
     string_of_num = "".join(list_of_numbers)
     phone_number = f"({string_of_num[:3]}){string_of_num[3:6]}-{string_of_num[6:]}"
     return phone_number
+
+
+# Test
+list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+phone_number = create_phone_number(list_of_numbers)
+print(phone_number)
