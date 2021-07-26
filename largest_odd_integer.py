17838484d271d70a8545c49663175f2744bd8243
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-07-26 15:46:03 -0400

    Amend and fix typos in print statements and inputs

--- largest_odd_integer.py
+++ largest_odd_integer.py
@@ -1,12 +1,12 @@
-numbers = input("Enter a list 10 integers separated by spaces: ")
+numbers = input("Enter a list of 10 integers separated by spaces: ")
 list_of_numbers = numbers.split()
 while len(list_of_numbers) < 10:
     numbers_needed = 10 - len(list_of_numbers)
-    additional_numbers = input(f"Please enter {numbers_needed} more numbers separated by spaces: ")
+    additional_numbers = input(f"Please enter {numbers_needed} more integers separated by spaces: ")
     list_of_numbers.extend(additional_numbers.split())
 if len(list_of_numbers) > 10:
     list_of_numbers = list_of_numbers[:10]
-    print("Too many integers we submitted. Only the first 10 will be used.")
+    print("Too many integers submitted. Only the first 10 were used.")
 largest_odd_number = 0
 for number in list_of_numbers:
     if int(number) % 2 == 1 and int(number) > largest_odd_number:
